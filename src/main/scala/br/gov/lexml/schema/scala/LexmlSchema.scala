72f49d1b02d266fd5f423eb8aa6f8d80f3abd518
João R. M. Nicola <joaoraf@gmail.com>
2023-11-30 16:11:34 -0300

    Migração para Scala 3 e Scalaxb 1.11.1

--- src/main/scala/br/gov/lexml/schema/scala/LexmlSchema.scala
+++ src/main/scala/br/gov/lexml/schema/scala/LexmlSchema.scala
@@ -1,13 +1,12 @@
 package br.gov.lexml.schema.scala
 
-import br.gov.lexml.schema.scala.data._
-import scala.xml._
+import br.gov.lexml.schema.scala.data.*
+import scala.xml.*
 
-
-object LexmlSchema {
-  def apply(e : scala.xml.Elem) : LexML = scalaxb.fromXML[LexML](e)
-  def apply(f : java.io.File) : LexML = apply(XML.loadFile(f))
-  def apply(d : Array[Byte]) : LexML = apply(XML.loadString(new String(d,"utf-8")))
-  def apply(src : String) : LexML = apply(XML.loadString(src)) 
-    
-}
+object LexmlSchema:
+  def apply(e: scala.xml.Elem): LexML = scalaxb.fromXML[LexML](e)
+  def apply(f: java.io.File): LexML = apply(XML.loadFile(f))
+  def apply(d: Array[Byte]): LexML = apply(
+    XML.loadString(new String(d, "utf-8"))
+  )
+  def apply(src: String): LexML = apply(XML.loadString(src))
